3c59c01d29866d10cb404f57fe63cb262857f81d
unesclabs102 <unesclabs102@LAB102A_24>
2022-12-01 17:17:25 -0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PnTela = new System.Windows.Forms.Panel();
+            this.LBPontuacao = new System.Windows.Forms.Label();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.menu = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.Frame = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnTela
+            // 
+            this.PnTela.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PnTela.Location = new System.Drawing.Point(12, 71);
+            this.PnTela.Name = "PnTela";
+            this.PnTela.Size = new System.Drawing.Size(428, 428);
+            this.PnTela.TabIndex = 0;
+
+            // 
+            // LBPontuacao
+            // 
+            this.LBPontuacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBPontuacao.Location = new System.Drawing.Point(12, 43);
+            this.LBPontuacao.Name = "LBPontuacao";
+            this.LBPontuacao.Size = new System.Drawing.Size(428, 23);
+            this.LBPontuacao.TabIndex = 0;
+            this.LBPontuacao.Text = "PONTOS: 0";
+            this.LBPontuacao.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(445, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // menu
+            // 
+            this.menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(50, 20);
+            this.menu.Text = "Menu";
+
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(134, 22);
+            this.toolStripMenuItem2.Text = "Iniciar Jogo";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(134, 22);
+            this.toolStripMenuItem3.Text = "Sair";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // Frame
+            // 
+            this.Frame.Tick += new System.EventHandler(this.Frame_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(445, 498);
+            this.Controls.Add(this.LBPontuacao);
+            this.Controls.Add(this.PnTela);
+            this.Controls.Add(this.menuStrip2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip2;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Clicado);
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel PnTela;
+        private System.Windows.Forms.Label LBPontuacao;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem menu;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+        private System.Windows.Forms.Timer Frame;
+    }
+}
+
---
+++ Snake.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    internal class Snake
+    {
+        public int Length { get; private set; }
+        public Point[] Location { get; private set; }
+
+
+        public Snake()
+        {
+            Location = new Point[28 * 28];
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Length = 5;
+            for(int i = 0; i < Length; i++)
+            {
+                Location[i].X = 12;
+                Location[i].Y = 12;
+            }
+        }
+
+        public void Follow()
+        {
+            for(int i = Length-1; i < 0; i--)
+            {
+                Location[i] = Location[i-1];
+            }
+        }
+
+        public void Up()
+        {
+            Location[0].Y--;
+            if(Location[0].Y < 0)
+            {
+                Location[0].Y += 28;
+            }
+            Follow();
+        }
+
+        public void Down()
+        {
+            Location[0].Y++;
+            if (Location[0].Y >27)
+            {
+                Location[0].Y -= 28;
+            }
+            Follow();
+        }
+
+        public void Left()
+        {
+            Location[0].X--;
+            if (Location[0].X < 0)
+            {
+                Location[0].X += 28;
+            }
+            Follow();
+        }
+
+        public void Rigth()
+        {
+            Location[0].X++;
+            if (Location[0].X > 27)
+            {
+                Location[0].X -= 28;
+            }
+            Follow();
+        }
+
+        public void Eat()
+        {
+            Length++;
+ 
+        }
+    }
+}
